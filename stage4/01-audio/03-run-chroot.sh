dddf8edf92fa885a2c49b6e91ede18c75810c003
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-16 19:06:03 +0200

    Added snapcast

--- stage4/01-audio/03-run-chroot.sh
+++ stage4/01-audio/03-run-chroot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+SNAPCAST_VERSION=0.31.0
+
 gpasswd -a pi audio
 
 install -m 755 -o mpd -g audio -d /var/lib/mpd/music
@@ -9,3 +11,15 @@ install -m 755 -o mpd -g audio -d /var/log/mpd
 chown -R mpd:audio /etc/mpd
 chown -R upmpdcli:audio /etc/upmpdcli
 chown -R root:audio /etc/raspotify
+
+SNAPSERVER=snapserver_${SNAPCAST_VERSION}-1_${ARCH}_${RELEASE}.deb
+
+wget -nc "https://github.com/badaix/snapcast/releases/download/v${SNAPCAST_VERSION}/${SNAPSERVER}"
+apt install --yes "./${SNAPSERVER}"
+rm -f "${SNAPSERVER}"
+
+SNAPCLIENT=snapclient_${SNAPCAST_VERSION}-1_${ARCH}_${RELEASE}.deb
+
+wget -nc "https://github.com/badaix/snapcast/releases/download/v${SNAPCAST_VERSION}/${SNAPCLIENT}"
+apt install --yes "./${SNAPCLIENT}"
+rm -f "${SNAPCLIENT}"
