09adf3e88c986530575ce2d639d7066873a8dd09
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-16 17:20:13 +0200

    Enabled wired networking

--- stage3/00-configure/00-run.sh
+++ stage3/00-configure/00-run.sh
@@ -5,6 +5,7 @@ install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d"          "files/97no-src
 install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d"          "files/97no-translation.conf"
 install -D -m 644 -t "${ROOTFS_DIR}/etc/sysctl.d"                "files/limit_kernel_logs.conf"
 install -D -m 644 -t "${ROOTFS_DIR}/etc/systemd/journald.conf.d" "files/disable_storage.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/systemd/network"         "files/20-eth0.network"
 
 on_chroot << EOF
 # Remove optional packages
