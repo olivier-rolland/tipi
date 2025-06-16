432c20e401007e562b71863173f63a955f3088b4
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-16 17:15:02 +0200

    Disabled translation packages

--- stage3/00-configure/00-run.sh
+++ stage3/00-configure/00-run.sh
@@ -2,6 +2,7 @@
 
 install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-recommends-suggests.conf"
 install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-src-pkg-cache.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-translation.conf"
 
 on_chroot << EOF
 # Remove optional packages
