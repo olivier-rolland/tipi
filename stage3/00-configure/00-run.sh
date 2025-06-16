ec177169c2606ef5e20a020f30331928e8892cf2
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-16 17:18:09 +0200

    Disabled journal storage

--- stage3/00-configure/00-run.sh
+++ stage3/00-configure/00-run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-recommends-suggests.conf"
-install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-src-pkg-cache.conf"
-install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-translation.conf"
-install -D -m 644 -t "${ROOTFS_DIR}/etc/sysctl.d"       "files/limit_kernel_logs.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d"          "files/97no-recommends-suggests.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d"          "files/97no-src-pkg-cache.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d"          "files/97no-translation.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/sysctl.d"                "files/limit_kernel_logs.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/systemd/journald.conf.d" "files/disable_storage.conf"
 
 on_chroot << EOF
 # Remove optional packages
