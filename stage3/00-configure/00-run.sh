46abc296624d2a525716fe7f04411e9387491ad0
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-16 17:16:32 +0200

    Limited kernel logs to console to warnings and errors

--- stage3/00-configure/00-run.sh
+++ stage3/00-configure/00-run.sh
@@ -3,6 +3,7 @@
 install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-recommends-suggests.conf"
 install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-src-pkg-cache.conf"
 install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d" "files/97no-translation.conf"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/sysctl.d"       "files/limit_kernel_logs.conf"
 
 on_chroot << EOF
 # Remove optional packages
