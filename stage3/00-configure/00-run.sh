e7a9952b2ed6987cbdee6ce64dc4e870ac0e7435
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-16 17:21:25 +0200

    Enabled ssh server discovery

--- stage3/00-configure/00-run.sh
+++ stage3/00-configure/00-run.sh
@@ -6,6 +6,7 @@ install -D -m 644 -t "${ROOTFS_DIR}/etc/apt/apt.conf.d"          "files/97no-tra
 install -D -m 644 -t "${ROOTFS_DIR}/etc/sysctl.d"                "files/limit_kernel_logs.conf"
 install -D -m 644 -t "${ROOTFS_DIR}/etc/systemd/journald.conf.d" "files/disable_storage.conf"
 install -D -m 644 -t "${ROOTFS_DIR}/etc/systemd/network"         "files/20-eth0.network"
+install -D -m 644 -t "${ROOTFS_DIR}/etc/avahi/services"          "files/ssh.service"
 
 on_chroot << EOF
 # Remove optional packages
