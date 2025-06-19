3f23f4723cc54f5e1e8af8d9510ab5d7d9dd3fa2
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-19 17:01:17 +0200

    Disabled audio services by default

--- stage3/01-audio/03-run-chroot.sh
+++ stage3/01-audio/03-run-chroot.sh
@@ -9,8 +9,17 @@ install -m 755 -o mpd -g audio -d /var/lib/mpd/playlists
 install -m 755 -o mpd -g audio -d /var/log/mpd
 
 chown -R mpd:audio /etc/mpd
+
+systemctl disable mpd.service
+systemctl disable mpd.socket
+
 chown -R upmpdcli:audio /etc/upmpdcli
+
+systemctl disable upmpdcli.service
+
 chown -R root:audio /etc/raspotify
+
+systemctl disable raspotify.service
 
 SNAPSERVER=snapserver_${SNAPCAST_VERSION}-1_${ARCH}_${RELEASE}.deb
 
@@ -18,8 +27,12 @@ wget -nc "https://github.com/badaix/snapcast/releases/download/v${SNAPCAST_VERSI
 apt install --yes "./${SNAPSERVER}"
 rm -f "${SNAPSERVER}"
 
+systemctl disable snapserver.service
+
 SNAPCLIENT=snapclient_${SNAPCAST_VERSION}-1_${ARCH}_${RELEASE}.deb
 
 wget -nc "https://github.com/badaix/snapcast/releases/download/v${SNAPCAST_VERSION}/${SNAPCLIENT}"
 apt install --yes "./${SNAPCLIENT}"
 rm -f "${SNAPCLIENT}"
+
+systemctl disable snapclient.service
