1c3d931c1a62017ca97dcf6b5221d40f6e193c41
Olivier Rolland <olivier.rolland@gmail.com>
2025-06-20 15:16:46 +0200

    Added default configuration files

--- stage3/01-audio/02-run.sh
+++ stage3/01-audio/02-run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
 install -D -m 644 -t "${ROOTFS_DIR}/lib/systemd/system" "files/mpd@.service"
+install -D -m 644 -t "${ROOTFS_DIR}/etc" "files/mpd.conf"
 install -D -m 644 "files/mpd-default.conf" "${ROOTFS_DIR}/etc/mpd/default.conf"
 
 install -D -m 644 -t "${ROOTFS_DIR}/lib/systemd/system" "files/upmpdcli@.service"
+install -D -m 644 -t "${ROOTFS_DIR}/etc" "files/upmpdcli.conf"
 install -D -m 644 "files/upmpdcli-default.conf" "${ROOTFS_DIR}/etc/upmpdcli/default.conf"
 
 install -D -m 644 -t "${ROOTFS_DIR}/lib/systemd/system" "files/raspotify@.service"
+install -D -m 644 "files/raspotify.conf" "${ROOTFS_DIR}/etc/raspotify/conf"
 install -D -m 644 "files/raspotify-default.conf" "${ROOTFS_DIR}/etc/raspotify/default.conf"
